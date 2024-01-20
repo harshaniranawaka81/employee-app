a5fd93fa347fc2f495c51abdc0eab8c67efef11e
Harshani Ranawaka <99527713+harshaniranawaka81@users.noreply.github.com>
2024-01-20 13:44:55 +0000

    commit obj

--- EmployeeApi/EmployeeApi/obj/Debug/net6.0/EmployeeApi.AssemblyInfo.cs
+++ EmployeeApi/EmployeeApi/obj/Debug/net6.0/EmployeeApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+743cfeaeb7fefac032bc09890ff2973218238d38")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
